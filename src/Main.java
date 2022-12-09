9db283a6f5f8a33eb63275140167a9d417279ddd
nurbek84 <nurbekkojjmberdiev84@gmail.com>
2022-12-09 16:44:00 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println("+");
+        int t = sc.nextInt();
+        int c = i+t;
+        System.out.println("=");
+        System.out.println(c);
+
+        }
+       // public static void main (int i, int t){
+
+        }
+
+
+
